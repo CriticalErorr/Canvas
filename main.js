8f15a9b1aa066996ef46e58a58fcf4f406c21b8c
CriticalErorr <costa84@gmail.com>
2020-01-19 03:45:31 +0200

    add class for spaceShip and FireBall

--- main.js
+++ main.js
@@ -6,72 +6,92 @@ const ctx = canvas.getContext('2d');
 let x = canvas.height / 2 - 30;
 let y = canvas.width - 50;
 
+// fire direction move control
+let startCanonBall = canvas.height - 70;
+let yDirectionShooting = 0;
 // space ship direction move control
 let xDirection = canvas.width / 2 - 30;
+
+class Space_Ship {
+    constructor() {
+        this.canvas = document.querySelector('canvas');
+        this.ctx = canvas.getContext('2d');
+        this.ctx.beginPath();
+        this.spaceship = this.ctx.rect(x, y, 60, 30);
+        this.ctx.strokeStyle = "green";
+        this.ctx.fillStyle = "green";
+        this.ctx.fill();
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        //// draw canon ////
+        this.canon = ctx.rect(x + 20, y - 20, 20, 20);
+        this.ctx.strokeStyle = "deeppink";
+        this.ctx.fillStyle = "deeppink";
+        this.ctx.fill();
+        this.ctx.stroke();
+        ////////////////////
+    }
+}
+
+class Fire_Ball {
+    constructor() {
+        this.canvas = document.querySelector('canvas');
+        this.ctx = canvas.getContext('2d');
+        this.ctx.beginPath();
+        this.canonBall = this.ctx.rect(x + 20, startCanonBall, 20, 20);
+        this.ctx.strokeStyle = "gold";
+        this.ctx.fillStyle = "gold";
+        this.ctx.fill();
+        this.ctx.stroke();
+    }
+}
+
+
 // event by pressing key
 document.onkeydown = checkKey;
 
 // function detect what presst
-function checkKey(e) {
+function checkKey(event) {
 
-    e = e || window.event;
+    event = event || window.event;
     // l key
-    if (e.keyCode == '37' && x >= 30) {
+    if (event.keyCode == '37' && x >= 30) {
         xDirection = xDirection - 10;
     }
     // r key
-    else if (e.keyCode == '39' && x <= canvas.width - 90) {
+    else if (event.keyCode == '39' && x <= canvas.width - 90) {
         xDirection = xDirection + 10;
     }
     // space
-    else if (e.keyCode == '32') {
+    else if (event.keyCode == '32') {
         yDirectionShooting = +5;
+        console.log(window.event);
     }
 
 }
-
-// fire direction move control
-let startCanonBall = canvas.height - 70;
-let yDirectionShooting = 0;
 
 //draw function
 function draw() {
     requestAnimationFrame(draw);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    // draw spaceship
-    ctx.beginPath();
-    let spaceship = ctx.rect(x, y, 60, 30);
-    ctx.strokeStyle = "green";
-    ctx.fillStyle = "green";
-    ctx.fill();
-    ctx.stroke();
-
-    // draw canonBall
-    ctx.beginPath();
-    let canonBall = ctx.rect(x + 20, startCanonBall, 20, 20);
-    ctx.strokeStyle = "gold";
-    ctx.fillStyle = "gold";
-    ctx.fill();
-    ctx.stroke();
-
-    // draw canon
-    ctx.beginPath();
-    let canon = ctx.rect(x + 20, y - 20, 20, 20);
-    ctx.strokeStyle = "deeppink";
-    ctx.fillStyle = "deeppink";
-    ctx.fill();
-    ctx.stroke();
+    ////draw spaceship////
+    new Fire_Ball(x, y);
+    new Space_Ship(x, y);
 
     x = xDirection
-
+    // restar fire
     if (startCanonBall < 10) {
         startCanonBall = canvas.height - 70;
         yDirectionShooting = 0;
     }
+    // fire direction up
     startCanonBall -= yDirectionShooting;
 
 }
 draw();
+
+
+
 
 
 
