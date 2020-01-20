51f61a6d671187610954afe2f27c53082aac4cf6
CriticalErorr <48606012+CriticalErorr@users.noreply.github.com>
2020-01-20 19:29:27 +0200

    new Version

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ const ctx = canvas.getContext('2d');
 //start position my spaceship
 let x = canvas.height / 2 - 30;
 let y = canvas.width - 50;
-
+let fire = [];
+let keyDown = {};
 // fire direction move control
 let startCanonBall = canvas.height - 70;
 let yDirectionShooting = 0;
@@ -38,7 +39,7 @@ class Fire_Ball {
         this.canvas = document.querySelector('canvas');
         this.ctx = canvas.getContext('2d');
         this.ctx.beginPath();
-        this.canonBall = this.ctx.rect(x + 20, startCanonBall, 20, 20);
+        this.ctx.rect(x + 20, startCanonBall, 20, 20);
         this.ctx.strokeStyle = "gold";
         this.ctx.fillStyle = "gold";
         this.ctx.fill();
@@ -48,36 +49,72 @@ class Fire_Ball {
 
 
 // event by pressing key
-document.onkeydown = checkKey;
+// document.onkeydown = checkKey;
+document.addEventListener('keydown', (event) => {
+    // function detect what presst
 
-// function detect what presst
-function checkKey(event) {
 
-    event = event || window.event;
+    // event = event || window.event;
     // l key
-    if (event.keyCode == '37' && x >= 30) {
-        xDirection = xDirection - 10;
+    if (event.keyCode == '37') {
+        keyDown['LEFT'] = true;
+       
     }
     // r key
-    else if (event.keyCode == '39' && x <= canvas.width - 90) {
-        xDirection = xDirection + 10;
+    else if (event.keyCode == '39') {
+        keyDown['RIGHT'] = true;
+        
     }
     // space
     else if (event.keyCode == '32') {
-        yDirectionShooting = +5;
-        console.log(window.event);
+        keyDown['SPACE'] = true;
+        fire.push(new Fire_Ball);
+        console.log(fire);
+    }
+});
+
+document.addEventListener('keyup', (eventup) => {
+    // l key
+    if (eventup.keyCode == '37') {
+        delete keyDown['LEFT'];
+        
+    }
+    // r key
+    else if (eventup.keyCode == '39') {
+        delete keyDown['RIGHT'];
+        
+    }
+    // space
+    else if (eventup.keyCode == '32') {
+        delete keyDown['SPACE'];
+       
+
     }
 
-}
+});
 
 //draw function
 function draw() {
     requestAnimationFrame(draw);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ////draw spaceship////
+    // for (let i = 0 ; i < fire.length ; i++){
+    //     return fire[i];
+    // }
     new Fire_Ball(x, y);
     new Space_Ship(x, y);
-
+    if ('RIGHT' in keyDown && x < canvas.width - 90) {
+        xDirection = xDirection + 10;
+    }
+    else if ('LEFT' in keyDown && x > 30) {
+        xDirection = xDirection - 10;
+    }
+    else if ('SPACE' in keyDown) {
+        for (let i = 0 ; i <fire.length ; i++){
+            return fire[i];
+        }
+        yDirectionShooting = +5;
+    }
     x = xDirection
     // restar fire
     if (startCanonBall < 10) {
@@ -89,7 +126,6 @@ function draw() {
 
 }
 draw();
-
 
 
 
